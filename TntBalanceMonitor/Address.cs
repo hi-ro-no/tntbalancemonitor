32ba22b5af39e4b46673a765006b8d3b302ef430
Hirono <32709712+watcher-hirono@users.noreply.github.com>
2018-02-16 10:42:42 +0900

    Initial Commit

---
+++ TntBalanceMonitor/Address.cs
@@ -0,0 +1,119 @@
+﻿// Copyright (C) 2017 valiant1x contact@intensecoin.com
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TntBalanceMonitor
+{
+    public class Address : INotifyPropertyChanged
+    {
+        string _Addr;
+        public string Addr
+        {
+            get { return _Addr; }
+            set
+            {
+                _Addr = value;
+                this.NotifyPropertyChanged();
+            }
+        }
+
+        string _Ip;
+        public string Ip
+        {
+            get { return _Ip; }
+            set
+            {
+                _Ip = value;
+                this.NotifyPropertyChanged();
+            }
+        }
+
+        string _Height;
+        public string Height
+        {
+            get { return _Height; }
+            set
+            {
+                _Height = value;
+                this.NotifyPropertyChanged();
+            }
+        }
+
+        bool _AuditPassed;
+        public bool AuditPassed
+        {
+            get { return _AuditPassed; }
+            set
+            {
+                _AuditPassed = value;
+                this.NotifyPropertyChanged();
+            }
+        }
+
+        float _BalEth;
+        public float BalEth
+        {
+            get { return _BalEth; }
+            set
+            {
+                _BalEth = value;
+                this.NotifyPropertyChanged();
+            }
+        }
+
+        float _BalTnt;
+        public float BalTnt
+        {
+            get { return _BalTnt; }
+            set
+            {
+                _BalTnt = value;
+                this.NotifyPropertyChanged();
+            }
+        }
+        DateTime _LastUpdated;
+        public DateTime LastUpdated
+        {
+            get { return _LastUpdated; }
+            set
+            {
+                _LastUpdated = value;
+                this.NotifyPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public Address(string addr, string ip)
+        {
+            Addr = addr;
+            Ip = ip;
+        }
+    }
+}
---
+++ TntBalanceMonitor/frmMain.cs
@@ -0,0 +1,193 @@
+﻿// Copyright (C) 2017 valiant1x contact@intensecoin.com
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+using System;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TntBalanceMonitor
+{
+    public partial class frmMain : Form
+    {
+        BindingList<Address> Addresses = new BindingList<Address>();
+        string strHttp = "http://";
+        string strConfig = "/config";
+
+        public frmMain()
+        {
+            InitializeComponent();
+            
+            if (File.Exists("address.txt"))
+            {
+                try
+                {
+                    var lines = File.ReadAllLines("address.txt");
+                    foreach(var line in lines)
+                    {
+                        if (string.IsNullOrEmpty(line))
+                            continue;
+
+                        if (Addresses.Where(x => x.Addr.Equals(line.Trim(), StringComparison.OrdinalIgnoreCase)).Count() == 0)
+                        {
+                            string[] t = line.Split('\t');
+                            Addresses.Add(new Address(t[0].Trim(), t[1].Trim()));
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+            else
+            {
+                try
+                {
+                    File.WriteAllText("address.txt", "");
+                }
+                catch { }              
+            }
+
+            AddLogMsg("API graciously powered by ethplorer.io");
+            AddLogMsg("Donations greatly appreciated: 0xa81248aE54dE6521d3afd58848006676EE874bFC");
+            AddLogMsg("Donations greatly appreciated (hirono): 0x463055B36E9e080cdB73eaF71744E75690c3D771");
+            AddLogMsg("Loaded " + Addresses.Count + " addresses to monitor.");
+            
+            dgMain.DataSource = null;
+            dgMain.AutoGenerateColumns = true;
+            dgMain.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            dgMain.AllowDrop =
+                dgMain.AllowUserToAddRows =
+                dgMain.AllowUserToDeleteRows =
+                false;
+
+            dgMain.ReadOnly = true;
+
+            dgMain.DataSource = new BindingSource { DataSource = Addresses };
+
+            var _ = CheckTokenBalance();
+        }
+
+        async Task CheckTokenBalance()
+        {
+            while (true)
+            {
+                bool wasFresh = false;
+
+                try
+                {
+                    //query server
+                    string url = @"https://api.ethplorer.io/getAddressInfo/{ADDR}?apiKey=freekey";
+                    var addr = Addresses.OrderBy(x => x.LastUpdated).First();
+                    url = url.Replace("{ADDR}", addr.Addr);
+
+                    wasFresh = (addr.LastUpdated == new DateTime());
+
+                    HttpWebRequest request = HttpWebRequest.CreateHttp(url);
+                    request.Timeout = 10000;
+                    var response = await request.GetResponseAsync();
+                    using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        string body = await reader.ReadToEndAsync();
+                        dynamic jsonResponse = Newtonsoft.Json.JsonConvert.DeserializeObject(body);
+
+                        addr.BalEth = jsonResponse.ETH.balance;
+
+                        float newTntBal = jsonResponse.tokens[0].balance / 100000000;
+
+                        if (addr.BalTnt != 0 &&
+                            addr.BalTnt != newTntBal)
+                            AddLogMsg("*** Value changedfor " + addr.Addr + " -- previous: " +
+                                addr.BalTnt + " -- new: " + newTntBal);
+
+                        addr.BalTnt = newTntBal;
+                        addr.LastUpdated = DateTime.Now;
+                    }
+
+                    //check node audit status
+                    string auditUrl = strHttp;
+
+                    auditUrl += addr.Ip + strConfig;
+
+                    addr.Height = "";
+                    addr.AuditPassed = false;
+
+                    request = HttpWebRequest.CreateHttp(auditUrl);
+                    request.Timeout = 10000;
+                    response = await request.GetResponseAsync();
+                    using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        string body = await reader.ReadToEndAsync();
+                        dynamic jsonResponse = Newtonsoft.Json.JsonConvert.DeserializeObject(body);
+
+                        string strTmp = jsonResponse.calendar.height.ToString();
+                        addr.Height = strTmp;
+                        addr.AuditPassed = true;
+                    }
+                }
+                catch { }
+                
+                dgMain.AutoResizeColumns();
+
+                await Task.Delay(wasFresh ? 15000 : 120000);
+            }
+        }
+
+        private void frmMain_Shown(object sender, EventArgs e)
+        {
+            if (Addresses.Count == 0)
+            {
+                MessageBox.Show("No addresses found! Put ETH addresses, IP address into address.txt file. 1 address per line.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                System.Diagnostics.Process.Start("address.txt");
+
+                this.Close();
+            }
+        }
+
+        void AddLogMsg(string msg)
+        {
+            txtLog.AppendText("[" + DateTime.Now.ToLongTimeString() + "] " + msg + Environment.NewLine);
+        }
+
+        private void dgMain_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            if (dgMain.Columns[e.ColumnIndex].Name == "BalTnt")
+            {
+                Address addr = Addresses[e.RowIndex];
+                if (addr.BalTnt >= 6000)
+                {
+                    e.CellStyle.BackColor = Color.Green;
+                    e.CellStyle.ForeColor = Color.WhiteSmoke;
+                }
+            }
+
+            if (dgMain.Columns[e.ColumnIndex].Name == "AuditPassed")
+            {
+                Address addr = Addresses[e.RowIndex];
+                if (!addr.AuditPassed)
+                {
+                    e.CellStyle.BackColor = Color.OrangeRed;
+                    e.CellStyle.ForeColor = Color.WhiteSmoke;
+                }
+            }
+        }
+    }
+}
